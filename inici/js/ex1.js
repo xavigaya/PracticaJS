b71eca334d13fad939e4a24bfd60dee900b3dcf0
xavi <xavi@Mac-mini-de-monica.local>
2020-04-27 17:00:28 +0200

    canvis

---
+++ inici/js/ex1.js
@@ -0,0 +1,27 @@
+document.write("Hola Món!");
+
+console.log("Hola classe");
+
+let a = 5;
+
+a++;
+
+document.write(a);
+
+a++;
+
+document.write(a);
+
+if (a == 7) {
+    document.write ("OK<br/>");
+} else {
+    document.write("fatal<br/>");
+}
+
+for (let index = 0; index < 5; index++) {
+    document.write("El valor de index és: " + index + "<br/>");
+}
+
+for (let index = 0; index < 5; index++) {
+    console.log("El valor de index és: " + index);
+}
